87502d84b2bbd6388d6eccb39fdc4e28ce8471e3
Valentin Sierra <valentinsierradw@gmail.com>
2026-07-01 10:53:28 -0300

    viteconfig para que corra en dist

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: "./index.html",
+                admin: "./admin.html",
+                about: "./about.html",
+                algorithms: "./algorithms.html"
+            }
+        }
+    }
+});
